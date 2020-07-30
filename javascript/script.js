e5bd2d230ccaf8e16bbad88905d46361a1263232
OmerHai <omer16493@gmail.com>
2020-07-30 15:36:16 +0300

    Add features page and download button redirect do gool play and app store

--- javascript/script.js
+++ javascript/script.js
@@ -54,3 +54,14 @@ function showSlides(imageNumber) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+function myFunction() {
+    if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) { window.location.href = 'https://itunes.apple.com/my/app/flipbizz/idexampleapp'; }
+
+    if (navigator.userAgent.toLowerCase().indexOf("android") > -1) { window.location.href = 'https://play.google.com/store/apps/details?id=com.exampleapp'; }
+
+    //Update #2
+    if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+        window.location.href = 'https://www.google.com'; //Desktop Browser
+    }
+}
